6fe929066bd286efca3e6eba8ad30ca359c03ea0
M Rajesh <m.rajeshrajz@gmail.com>
2019-07-04 19:08:51 +0530

    bug fixes and better naming convensions

--- looping/SnakeAndLaddder.py
+++ looping/SnakeAndLaddder.py
@@ -1,48 +1,48 @@
 import random as r
-userChoice=0
-systemChoice=0
-SH=65
-ST=30
+user_choice=0
+system_choice=0
+SH=45
+ST=25
 LH=85
-LT=35
-userOneCurrentLocation=0
-userTwoCurrentLocation=0
+LT=27
+user_one_current_location=0
+user_two_current_location=0
 
-def findUserCurrentLocation(userInput,userCurrentLocation):
-    if (userInput+userCurrentLocation)==LT:
+def find_user_current_location(user_input, user_current_location):
+    if (user_input + user_current_location)==LT:
         print("Ohh thats great you took ladder Congrats !!!")
         return LH
-    elif (userInput+userCurrentLocation)==SH:
+    elif (user_input + user_current_location)==SH:
         print("you have been bit by snake")
         return ST
     else:
-        return userCurrentLocation+userInput
+        return user_current_location + user_input
 
 while True:
     while True:
-        userChoice=int(input("Enter your value of dice :"))
-        if userChoice>=1 and userChoice <=6:
+        user_choice=int(input("Enter your value of dice :"))
+        if 1 <= user_choice <= 6: # userChoice >=1 and userChoice <= 6
             break
         else:
             print("Dice value should only between 1 to 6 ")
-    print(f"User one input is {userChoice}")
-    userChoice=findUserCurrentLocation(userChoice,userOneCurrentLocation)
-    userOneCurrentLocation=userChoice
-    if userOneCurrentLocation >=100:
-        print("User one win")
+    print(f"User input is {user_choice}")
+    user_choice=find_user_current_location(user_choice, user_one_current_location)
+    user_one_current_location=user_choice
+    if user_one_current_location >=100:
+        print("User win")
         break
     else:
-        print(f"User one at postion{userOneCurrentLocation}")
-    systemChoice=r.randint(1,6)
-    print(f"system input is {systemChoice}")
-    systemChoice=findUserCurrentLocation(systemChoice,userTwoCurrentLocation)
-    userTwoCurrentLocation=systemChoice
-    if userTwoCurrentLocation>=100:
+        print(f"User position at {user_one_current_location} ")
+    system_choice=r.randint(1, 6)
+    print(f"System input is {system_choice}")
+    system_choice=find_user_current_location(system_choice, user_two_current_location)
+    user_two_current_location=system_choice
+    if user_two_current_location>=100:
         print("System won")
         break
 
     else:
-        print(f"System at postion {userTwoCurrentLocation}")
+        print(f"System position at {user_two_current_location}")
 
 
 
