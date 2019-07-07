516d405d4db646049260efbc14357c5c0e302a1b
M Rajesh <m.rajeshrajz@gmail.com>
2019-07-08 01:36:51 +0530

    improved efficency

--- Day1/primenumber.py
+++ Day1/primenumber.py
@@ -1,10 +1,13 @@
+import math
+
 def isPrime(n):
     if n <2:
         return False
     elif n==2:
         return True
     else:
-        for i in range(2,n):
+        max=math.floor(math.sqrt(n))
+        for i in range(2,max+1):
             if n%2==0:
                 return False
         return True
@@ -12,4 +15,4 @@ def isPrime(n):
 
 num = int(input("Enter a number to check prime or not :"))
 
-print(f"{num} Prime number" if isPrime(num) else "{num} Not prime Number")
+print(f"{num} Prime number" if isPrime(num) else " {num} Not prime Number")
