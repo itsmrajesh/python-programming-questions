5b91e6f3cd1a29379b46ca3e9c8269f2f7276b9d
M Rajesh <m.rajeshrajz@gmail.com>
2019-07-20 01:01:53 +0530

    Group Anagrams

---
+++ PythonDataStructures/GroupAnagrams.py
@@ -0,0 +1,15 @@
+def group_anagrams(lst):
+    dct=dict()
+    for ele in lst:
+        s = ''.join(sorted(ele))
+        if dct.get(s):
+            dct[s].append(ele)
+        else:
+            dct[s] = [ele]
+
+    for k,v in dct.items():
+        print(k)
+        print(v)
+
+lst=["eat","tea","tan","ate","nat","bat"]
+print(group_anagrams(lst))
