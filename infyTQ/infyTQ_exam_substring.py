c43d919f9f420e78c63f10b6c8a1e9e084f6078e
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-10 17:52:44 +0530

    added sample I/O

--- infyTQ/infyTQ_exam_substring.py
+++ infyTQ/infyTQ_exam_substring.py
@@ -1,4 +1,9 @@
-"""Problem Statement https://www.geeksforgeeks.org/infosys-certification-exam-infytq/"""
+''' Problem Statement https://www.geeksforgeeks.org/infosys-certification-exam-infytq/ '''
+
+''' ex : input: abcabcbb      output :- abc
+    ex : input: abcabcdedadcb output :- abcde  
+'''
+
 
 def is_sub(s):
     if len(s) >= 3:
@@ -14,16 +19,17 @@ def get_substring(s):
     l = len(s)
     min = 3
     max = 0
-    lst=list()
+    lst = list()
     for i in range(0, l):
-        for j in range(i + 3, l):
-            sub = s[i:j + 1]
+        for j in range(i + 3, l + 1):
+            sub = s[i:j]
             # print(sub)
             if is_sub(sub):
                 if len(sub) > max:
+                    # print(sub)
                     max = len(sub)
                     lst.append(sub)
-    if lst==[]:
+    if lst == []:
         return -1
     else:
         return lst[-1]
@@ -31,3 +37,8 @@ def get_substring(s):
 
 s = input()
 print(get_substring(s))
+# st = set(s)
+# if len(st) > 1:
+#     print(get_substring(s))
+# else:
+#     print(st.pop())
