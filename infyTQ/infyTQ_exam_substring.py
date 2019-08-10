c707e09c2190f62d86ef65cadebda2ce7d988ca6
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-10 14:44:50 +0530

    added substring infyTQ Question

---
+++ infyTQ/infyTQ_exam_substring.py
@@ -0,0 +1,34 @@
+longest=0
+
+def is_sub(s):
+    if len(s)>=3:
+        for word in s:
+            if (s.count(word)>1):
+                return False
+
+        longest=len(s)
+        return True
+    else:
+        return False
+
+def get_substring(s):
+    l=len(s)
+    min=3
+    max=0
+    res=""
+    for i in range(0,l):
+        for j in range(i+3,l-3):
+            sub=s[i:j]
+            if is_sub(sub):
+                if len(sub)>max:
+                    max=len(sub)
+                    res=sub
+    return res
+
+
+
+
+
+
+s=input()
+print(get_substring(s))
