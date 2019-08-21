ba96e0c3720d9aa1f0255c24f9df2129b30885b5
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-21 15:25:05 +0530

    added NPTEL week 3 assignment

--- NPTEL/week_3_assignment.py
+++ NPTEL/week_3_assignment.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def expanding(lst):
     size = len(lst)
     res = False
@@ -37,6 +36,7 @@ def is_accordain_dec(lst):
                 return False
     return res
 
+
 def is_accordain_inc(lst):
     size = len(lst)
     diff, max, res = 0, 0, False
@@ -54,24 +54,28 @@ def is_accordain_inc(lst):
                 return False
     return res
 
+
 def accordian(lst):
     diff = 0
     abs_lst = []
-    res=False
+    res = False
     for i in range(1, len(lst)):
         diff = abs(lst[i - 1] - lst[i])
         abs_lst.append(diff)
     for i in range(0, len(abs_lst) - 2):
         if abs_lst[i] < abs_lst[i + 1] and abs_lst[i + 1] > abs_lst[i + 2]:
-            continue
+            res = True
         elif abs_lst[i] > abs_lst[i + 1] and abs_lst[i + 1] < abs_lst[i + 2]:
-            continue
+            res = True
         else:
             return False
-    return True
+    if res:
+        return True
+    else:
+        return False
 
 
-print(accordian([-2, 1, 5, 2, 8, 3,]))
+print(accordian([-2, 1, 5, 2, 8, 3, ]))
 
 
 def rotate(arr):
