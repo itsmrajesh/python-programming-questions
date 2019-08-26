0c00d4361aab911764e293d332d9450ba43a73c3
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-27 00:27:30 +0530

    updated juspay string problem

--- juspay_string_1.py
+++ juspay_string_1.py
@@ -1,3 +1,11 @@
+"""
+Given String s = 1234
+possible substrings of "1234" are 1,2,3,4,12,23,34,123,234 and 1234
+Sum S = 1+2+3+4+12+23+34+123+234+1234=1670
+Reverse R = 761
+"""
+
+
 s = input()
 size = len(s)
 sum = 0
