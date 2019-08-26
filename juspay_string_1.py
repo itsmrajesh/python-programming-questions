2c8604c3a1a64cc7cc4e0488ef3ea0e44e9f29a0
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-27 00:33:16 +0530

    updated juspay interview  string problem

--- juspay_string_1.py
+++ juspay_string_1.py
@@ -1,10 +1,9 @@
 """
-Given String s = 1234
+Given String s = 1234 and s > 1
 possible substrings of "1234" are 1,2,3,4,12,23,34,123,234 and 1234
 Sum S = 1+2+3+4+12+23+34+123+234+1234=1670
 Reverse R = 761
 """
-
 
 s = input()
 size = len(s)
@@ -15,12 +14,12 @@ for i in range(0, size):
     print(s[i], end=" ")
     sum += int(s[i])
     k = c
-    for j in range(size - c+1):
+    for j in range(size - c + 1):
         print(s[j:k], end=" ")
         sum += int(s[j:k])
         k += 1
     c += 1
 print()
-print("Sum:",sum)
-s=str(sum)[::-1] #reverse the string
-print("Sum Reverse:",int(s))
+print("Sum:", sum)
+s = str(sum)[::-1]  # reverse the string
+print("Sum Reverse:", int(s))
